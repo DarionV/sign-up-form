dfd2ca18328cb4b1b5513ae6309284f7ef8ff463
Darion Valdez <Darionvldz@gmail.com>
2024-01-19 19:48:13 +0100

    add password validation

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const submitButton = document.querySelector('.create-account-button');
 const form = document.querySelector('#form');
 const inputs = document.querySelectorAll('input');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const passwordErrorMessage = document.querySelector('.password-error-message');
+
+confirmPassword.addEventListener('keyup', checkPass);
+passwordErrorMessage.style.display = 'none';
 
 inputs.forEach((input)=>{
     input.addEventListener('invalid', validate);
@@ -8,4 +14,12 @@ inputs.forEach((input)=>{
 
 function validate(){
    form.classList.add('submitted');
+}
+
+function checkPass(){
+    if(password.value !== confirmPassword.value) {
+        passwordErrorMessage.style.display = 'block';
+    } else {
+        passwordErrorMessage.style.display = 'none';
+    }
 }
